0aab0b6e67c114f57643917d9f787d5f56a078be
Min Xu <xukmin@gmail.com>
2018-06-06 09:15:04 -0700

    Add more activation functions.
    Also add linked diagrams of individual activation functions.

--- activation-functions/README.md
+++ activation-functions/README.md
@@ -1,6 +1,9 @@
 Activation Functions
 ====================
 
+Interative visualization of various activation functions used in neural
+networks.
+
 ## References
 
 - Activation Function, https://en.wikipedia.org/wiki/Activation_function
--- activation-functions/activation-functions.js
+++ activation-functions/activation-functions.js
@@ -1,30 +1,33 @@
 'use strict';
 
 function drawActivationFunctions() {
+  functionPlot.globals.COLORS = [...functionPlot.globals.COLORS, ...functionPlot.globals.COLORS, ...functionPlot.globals.COLORS, ...functionPlot.globals.COLORS];
+  const widthIndividual = 480;
+  const heightIndividual = 240;
+
   const target = document.getElementById('root');
 
   const identity = {
     title: 'Identity',
     fn: 'x'
   };
-  const binaryStepLeft = {
-    title: 'Binary Step',
+  const binaryStep = [{
+    title: 'BinaryStep',  // 'Binary Step',
     fn: '0',
     range: [-Infinity, 0],
     color: 'red',
-  };
-  const binaryStepRight = {
-    title: 'Binary Step',
+  }, {
+    title: 'BinaryStep',  // 'Binary Step',
     fn: '1',
     range: [0, Infinity],
     color: 'red',
-  };
+  }];
   const logSigmoid = {
-    title: 'Log Sigmoid',
+    title: 'LogSigmoid',  // 'Log Sigmoid',
     fn: '1 / (1 + exp(-x))',
   };
   const tanhSigmoid = {
-    title: 'Hyperbolic Tangent Sigmoid',
+    title: 'TanhSigmoid',  // 'Hyperbolic Tangent Sigmoid',
     fn: '(exp(x) - exp(-x)) / (exp(x) + exp(-x))',
   };
   const arcTan = {
@@ -39,15 +42,49 @@ function drawActivationFunctions() {
     title: 'SoftPlus',
     fn: 'ln(1 + exp(x))',
   };
-  const reLULeft= {
+  const reLU = [{
     title: 'ReLU',
     fn: '0',
     range: [-Infinity, 0],
-  };
-  const reLURight = {
+    color: 'blue',
+  }, {
     title: 'ReLU',
     fn: 'x',
     range: [0, Infinity],
+    color: 'blue',
+  }];
+  const bentIdentity = {
+    title: 'BentIdentity',
+    fn: '(sqrt(x ^ 2 + 1) - 1) / 2 + x',
+  };
+  const iSRUs = [];
+  for (let alpha of [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]) {
+    iSRUs.push({
+      title: 'ISRU',
+      fn: `x / sqrt(1 + ${alpha} * x ^ 2)`,
+    });
+  }
+  const softExponentials = [];
+  for (let alpha of [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]) {
+    softExponentials.push({
+      title: 'SoftExponential',
+      fn: alpha < 0 ?
+          `-ln(1 - ${alpha} * (x + ${alpha})) / ${alpha}` :
+          alpha == 0 ? 'x' :
+          `(exp(${alpha} * x) - 1) / ${alpha} + ${alpha}`,
+    });
+  }
+  const sinusoid = {
+    title: 'Sinusoid',
+    fn: 'sin(x)',
+  };
+  const sinc = {
+    title: 'Sinc',
+    fn: 'sin(x) / x',
+  };
+  const gaussian = {
+    title: 'Gaussian',
+    fn: 'exp(- x ^ 2)',
   };
   functionPlot({
     title: 'Activation Functions',
@@ -55,14 +92,62 @@ function drawActivationFunctions() {
     height: 768,
     data: [
       identity,
-      binaryStepLeft, binaryStepRight,
+      ...binaryStep,
       logSigmoid,
       tanhSigmoid,
       arcTan,
       softSign,
       softPlus,
-      //reLULeft, reLURight,
+      ...reLU,
+      bentIdentity,
+      // ...iSRUs,
+      // ...softExponentials,
+      sinusoid,
+      //sinc,
+      gaussian,
     ],
     target
   });
+
+  function plot(datum) {
+    let data = Array.isArray(datum) ? datum : [datum];
+    const element = document.createElement('div');
+    element.className = 'individual';
+    element.id = data[0].title;
+    element.style.width = `${widthIndividual}px`;
+    element.style.height = `${heightIndividual}px`;
+    document.body.appendChild(element);
+    return functionPlot({
+      width: widthIndividual,
+      height: heightIndividual,
+      data: data,
+      title: element.id,
+      target: `#${element.id}`,
+    });
+  }
+
+  const plots = [
+    plot(identity),
+    plot(binaryStep),
+    plot(logSigmoid),
+    plot(tanhSigmoid),
+    plot(arcTan),
+    plot(softSign),
+    plot(softPlus),
+    plot(reLU),
+    plot(bentIdentity),
+    plot(iSRUs),
+    plot(softExponentials),
+    plot(sinusoid),
+    //plot(sinc),
+    plot(gaussian),
+  ];
+
+  for (let p of plots) {
+    for (let q of plots) {
+      if (p != q) {
+        p.addLink(q);
+      }
+    }
+  }
 }
